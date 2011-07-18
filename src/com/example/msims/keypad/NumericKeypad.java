48b66a5e64b724e28d56fd5730c00c1658e7a585
Michael Sims <michael.h.sims@gmail.com>
2011-07-18 16:24:40 -0500

    For now just put up a Toast of which button was pressed

--- src/com/example/msims/keypad/NumericKeypad.java
+++ src/com/example/msims/keypad/NumericKeypad.java
@@ -3,7 +3,13 @@ package com.example.msims.keypad;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NumericKeypad extends RelativeLayout {
 
@@ -23,9 +29,25 @@ public class NumericKeypad extends RelativeLayout {
         init(context);
     }
 
-    private void init(Context context) {
+    private void init(final Context context) {
         final LayoutInflater layoutInflater = LayoutInflater.from(context);
         layoutInflater.inflate(R.layout.numeric_keypad, this, true);
+
+        final Integer[] buttons = {R.id.keypadZero, R.id.keypadOne, R.id.keypadTwo, R.id.keypadThree, R.id.keypadFour,
+                                   R.id.keypadFive, R.id.keypadSix, R.id.keypadSeven, R.id.keypadEight, R.id.keypadNine,
+                                   R.id.keypadBackspace};
+
+        final OnClickListener listener = new OnClickListener() {
+            public void onClick(final View view) {
+                final Toast toast = Toast.makeText(context, view.getTag().toString(), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        };
+        for (Integer viewId : buttons) {
+            TextView view = (TextView) findViewById(viewId);
+            view.setTag(view.getText());
+            view.setOnClickListener(listener);
+        }
     }
 
 }
