1ee6d5b2cc767e1d715a431c3690deee39f40f3b
Michael Sims <michael.h.sims@gmail.com>
2011-07-19 10:03:00 -0500

    Make a PinKeypad dialog fragment

--- src/com/example/msims/keypad/MainActivity.java
+++ src/com/example/msims/keypad/MainActivity.java
@@ -2,20 +2,21 @@ package com.example.msims.keypad;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements NumericKeypad.KeyPressedListener {
+public class MainActivity extends FragmentActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        NumericKeypad keypad = (NumericKeypad) findViewById(R.id.keypad);
-        keypad.setKeyPressedListener(this);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(new PinKeypadFragment(), null);
+        ft.commit();
     }
 
-    public void onKeyPressed(final String key) {
-        Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
-    }
 }
---
+++ src/com/example/msims/keypad/PinKeypadFragment.java
@@ -0,0 +1,30 @@
+package com.example.msims.keypad;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class PinKeypadFragment extends DialogFragment implements NumericKeypad.KeyPressedListener {
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Disable any automatic styling */
+        setStyle(android.app.DialogFragment.STYLE_NO_FRAME, 0);
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.pin_keypad, container, false);
+        ((NumericKeypad) layout.findViewById(R.id.keypad)).setKeyPressedListener(this);
+        return layout;
+    }
+
+    public void onKeyPressed(final String key) {
+        Toast.makeText(getActivity(), key, Toast.LENGTH_SHORT).show();
+    }
+}
