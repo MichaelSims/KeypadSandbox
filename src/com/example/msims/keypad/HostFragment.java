c8a31da7c6bfd8557b26fa984eabcf6ca19d30e8
Michael Sims <michael.h.sims@gmail.com>
2011-07-20 09:00:39 -0500

    Whitespace fix

--- src/com/example/msims/keypad/HostFragment.java
+++ src/com/example/msims/keypad/HostFragment.java
@@ -9,7 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HostFragment extends Fragment{
+public class HostFragment extends Fragment {
 
     @Override
     public void onAttach(final Activity activity) {
